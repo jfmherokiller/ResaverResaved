7744c58b1acf6d18141914178492e133bcc1e8e4
noah the goodra <peterpan0413@live.com>
2021-05-02 15:44:53 -0500

    convert Reference to kotlin

--- src/main/java/resaver/ess/papyrus/Reference.kt
+++ src/main/java/resaver/ess/papyrus/Reference.kt
@@ -13,321 +13,272 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package resaver.ess.papyrus;
+package resaver.ess.papyrus
 
-import resaver.ListException;
-import resaver.ess.AnalyzableElement;
-import java.util.Optional;
-import java.util.SortedSet;
-import java.nio.ByteBuffer;
-import java.util.Collections;
-import java.util.List;
-import resaver.ess.Element;
-import resaver.ess.ESS;
-import resaver.ess.Linkable;
+import resaver.Analysis
+import resaver.ListException
+import resaver.ess.ESS
+import resaver.ess.Element
+import resaver.ess.Linkable
+import resaver.ess.papyrus.Script.Companion.NATIVE_SCRIPTS
+import java.nio.ByteBuffer
+import java.util.*
+import java.util.function.Consumer
+import kotlin.experimental.and
 
 /**
  * Describes a reference in a Skyrim savegame.
  *
  * @author Mark Fairchild
  */
-final public class Reference extends GameElement implements SeparateData, HasVariables {
-
+class Reference
+/**
+ * Creates a new `Reference` by reading from a
+ * `ByteBuffer`. No error handling is performed.
+ *
+ * @param input The input stream.
+ * @param scripts The `ScriptMap` containing the definitions.
+ * @param context The `PapyrusContext` info.
+ */
+internal constructor(input: ByteBuffer?, scripts: ScriptMap?, context: PapyrusContext?) :
+    GameElement(input, scripts, context), SeparateData, HasVariables {
     /**
-     * Creates a new <code>Reference</code> by reading from a
-     * <code>ByteBuffer</code>. No error handling is performed.
-     *
-     * @param input The input stream.
-     * @param scripts The <code>ScriptMap</code> containing the definitions.
-     * @param context The <code>PapyrusContext</code> info.
-     */
-    Reference(ByteBuffer input, ScriptMap scripts, PapyrusContext context) throws PapyrusFormatException {
-        super(input, scripts, context);
-    }
-
-    /**
-     * @see resaver.ess.Element#write(resaver.ByteBuffer)
+     * @see resaver.ess.Element.write
      * @param output The output stream.
      */
-    @Override
-    public void write(ByteBuffer output) {
-        super.write(output);
+    override fun write(output: ByteBuffer) {
+        super.write(output)
     }
 
     /**
-     * @see SeparateData#readData(java.nio.ByteBuffer,
-     * resaver.ess.papyrus.PapyrusContext)
+     * @see SeparateData.readData
      * @param input
      * @param context
      * @throws PapyrusElementException
      * @throws PapyrusFormatException
      */
-    @Override
-    public void readData(ByteBuffer input, PapyrusContext context) throws PapyrusElementException, PapyrusFormatException {
-        this.data = new ReferenceData(input, context);
+    @Throws(PapyrusElementException::class, PapyrusFormatException::class)
+    override fun readData(input: ByteBuffer, context: PapyrusContext) {
+        data = ReferenceData(input, context)
     }
 
     /**
-     * @see SeparateData#writeData(java.nio.ByteBuffer)
-     * @param output
+     * @see SeparateData.writeData
+     * @param input
      */
-    @Override
-    public void writeData(ByteBuffer output) {
-        this.data.write(output);
+    override fun writeData(input: ByteBuffer) {
+        data!!.write(input)
     }
 
     /**
-     * @see resaver.ess.Element#calculateSize()
-     * @return The size of the <code>Element</code> in bytes.
+     * @see resaver.ess.Element.calculateSize
+     * @return The size of the `Element` in bytes.
      */
-    @Override
-    public int calculateSize() {
-        int sum = super.calculateSize();
-        sum += this.data == null ? 0 : this.data.calculateSize();
-        return sum;
+    override fun calculateSize(): Int {
+        var sum = super.calculateSize()
+        sum += if (data == null) 0 else data!!.calculateSize()
+        return sum
     }
 
     /**
-     * @return The <code>ReferenceData</code> for the instance.
+     * @return The name of the corresponding `Script`.
      */
-    public ReferenceData getData() {
-        return this.data;
-    }
+    val scriptName: TString
+        get() = super.getDefinitionName()
 
     /**
-     * Sets the data field.
-     *
-     * @param newData The new value for the data field.
+     * @return The corresponding `Script`.
      */
-    public void setData(ReferenceData newData) {
-        this.data = newData;
-    }
-
-    /**
-     * @return The name of the corresponding <code>Script</code>.
-     */
-    public TString getScriptName() {
-        return super.getDefinitionName();
-    }
-
-    /**
-     * @return The corresponding <code>Script</code>.
-     */
-    public Script getScript() {
-        assert super.getDefinition() instanceof Script;
-        return (Script) super.getDefinition();
-    }
-
-    /**
-     * @return A flag indicating if the <code>Reference</code> is undefined.
-     *
-     */
-    @Override
-    public boolean isUndefined() {
-        if (null != this.getScript()) {
-            return this.getScript().isUndefined();
+    val script: Script?
+        get() {
+            assert(super.getDefinition() is Script)
+            return super.getDefinition() as Script
         }
 
-        return !Script.Companion.getNATIVE_SCRIPTS().contains(this.getScriptName().toIString());
+    /**
+     * @return A flag indicating if the `Reference` is undefined.
+     */
+    override fun isUndefined(): Boolean {
+        return if (null != script) {
+            script!!.isUndefined
+        } else !NATIVE_SCRIPTS.contains(scriptName.toIString())
     }
 
     /**
      * @return The type of the reference.
      */
-    public TString getType() {
-        return null == this.data ? null : this.data.TYPE;
-    }
+    val type: TString?
+        get() = if (null == data) null else data!!.TYPE
 
     /**
-     * @see HasVariables#getVariables()
+     * @see HasVariables.getVariables
      * @return
      */
-    @Override
-    public List<Variable> getVariables() {
-        return this.data == null
-                ? Collections.emptyList()
-                : Collections.unmodifiableList(this.data.VARIABLES);
+    override fun getVariables(): List<Variable> {
+        return if (data == null) emptyList() else Collections.unmodifiableList(data!!.VARIABLES)
     }
 
     /**
-     * @see HasVariables#getDescriptors()
+     * @see HasVariables.getDescriptors
      * @return
      */
-    @Override
-    public List<MemberDesc> getDescriptors() {
-        return this.getScript().getExtendedMembers();
+    override fun getDescriptors(): List<MemberDesc> {
+        return script!!.extendedMembers
     }
 
     /**
-     * @see HasVariables#setVariable(int, resaver.ess.papyrus.Variable)
+     * @see HasVariables.setVariable
      * @param index
      * @param newVar
      */
-    @Override
-    public void setVariable(int index, Variable newVar) {
-        if (this.data == null || this.data.VARIABLES == null) {
-            throw new NullPointerException("The variable list is missing.");
+    override fun setVariable(index: Int, newVar: Variable) {
+        if (data == null || data!!.VARIABLES == null) {
+            throw NullPointerException("The variable list is missing.")
         }
-        if (index <= 0 || index >= this.data.VARIABLES.size()) {
-            throw new IllegalArgumentException("Invalid variable index: " + index);
-        }
-
-        this.data.VARIABLES.set(index, newVar);
+        require(!(index <= 0 || index >= data!!.VARIABLES!!.size)) { "Invalid variable index: $index" }
+        data!!.VARIABLES!![index] = newVar
     }
 
     /**
-     * @see resaver.ess.Linkable#toHTML(Element)
-     * @param target A target within the <code>Linkable</code>.
+     * @see resaver.ess.Linkable.toHTML
+     * @param target A target within the `Linkable`.
      * @return
      */
-    @Override
-    public String toHTML(Element target) {
+    override fun toHTML(target: Element?): String {
         if (null != target) {
-            if (target instanceof Variable) {
-                int i = this.getVariables().indexOf(target);
+            if (target is Variable) {
+                val i = this.variables.indexOf(target)
                 if (i >= 0) {
-                    return Linkable.makeLink("reference", this.getID(), i, this.toString());
+                    return Linkable.makeLink("reference", this.id, i, this.toString())
                 }
-
             } else {
-                Optional<Variable> result = this.getVariables().stream()
-                        .filter(Variable::hasRef)
-                        .filter(v -> v.getReferent() == target)
-                        .findFirst();
-
-                if (result.isPresent()) {
-                    int i = this.getVariables().indexOf(result.get());
+                val result = this.variables.stream()
+                    .filter { obj: Variable -> obj.hasRef() }
+                    .filter { v: Variable -> v.referent === target }
+                    .findFirst()
+                if (result.isPresent) {
+                    val i = this.variables.indexOf(result.get())
                     if (i >= 0) {
-                        return Linkable.makeLink("reference", this.getID(), i, this.toString());
+                        return Linkable.makeLink("reference", this.id, i, this.toString())
                     }
                 }
             }
         }
-
-        return Linkable.makeLink("reference", this.getID(), this.toString());
+        return Linkable.makeLink("reference", this.id, this.toString())
     }
 
     /**
-     * @see AnalyzableElement#getInfo(resaver.Analysis, resaver.ess.ESS)
+     * @see AnalyzableElement.getInfo
      * @param analysis
      * @param save
      * @return
      */
-    @Override
-    public String getInfo(resaver.Analysis analysis, ESS save) {
-        final StringBuilder BUILDER = new StringBuilder();
-        if (null != this.getScript()) {
-            BUILDER.append(String.format("<html><h3>REFERENCE of %s</h3>", this.getScript().toHTML(this)));
+    override fun getInfo(analysis: Analysis, save: ESS): String {
+        val BUILDER = StringBuilder()
+        if (null != script) {
+            BUILDER.append(String.format("<html><h3>REFERENCE of %s</h3>", script!!.toHTML(this)))
         } else {
-            BUILDER.append(String.format("<html><h3>REFERENCE of %s</h3>", this.getScriptName()));
+            BUILDER.append(String.format("<html><h3>REFERENCE of %s</h3>", scriptName))
         }
-
-        if (null != analysis) {
-            SortedSet<String> providers = analysis.SCRIPT_ORIGINS.get(this.getScriptName().toIString());
-            if (null != providers) {
-                String probablyProvider = providers.last();
-                BUILDER.append(String.format("<p>This script probably came from \"%s\".</p>", probablyProvider));
-
-                if (providers.size() > 1) {
-                    BUILDER.append("<p>Full list of providers:</p><ul>");
-                    providers.forEach(mod -> BUILDER.append(String.format("<li>%s", mod)));
-                    BUILDER.append("</ul>");
-                }
+        val providers = analysis.SCRIPT_ORIGINS[scriptName.toIString()]
+        if (null != providers) {
+            val probablyProvider = providers.last()
+            BUILDER.append(String.format("<p>This script probably came from \"%s\".</p>", probablyProvider))
+            if (providers.size > 1) {
+                BUILDER.append("<p>Full list of providers:</p><ul>")
+                providers.forEach(Consumer { mod: String? -> BUILDER.append(String.format("<li>%s", mod)) })
+                BUILDER.append("</ul>")
             }
         }
-
-        BUILDER.append(String.format("<p>ID: %s</p>", this.getID()));
-        BUILDER.append(String.format("<p>Type2: %s</p>", this.getType()));
-        BUILDER.append(String.format("<p>Unknown1: %08x</p>", this.data.UNKNOWN1));
-        BUILDER.append(String.format("<p>Unknown2: %08x</p>", this.data.UNKNOWN2));
-
-        Linkable UNKNOWN1 = save.getPapyrus().getContext().broadSpectrumSearch(this.getData().UNKNOWN1);
+        BUILDER.append(String.format("<p>ID: %s</p>", this.id))
+        BUILDER.append(String.format("<p>Type2: %s</p>", type))
+        BUILDER.append(String.format("<p>Unknown1: %08x</p>", data!!.UNKNOWN1))
+        BUILDER.append(String.format("<p>Unknown2: %08x</p>", data!!.UNKNOWN2))
+        val UNKNOWN1 = save.papyrus.context.broadSpectrumSearch(data!!.UNKNOWN1)
         if (null != UNKNOWN1) {
-            BUILDER.append("<p>Potential match for unknown1 found using general search:<br/>");
-            BUILDER.append(UNKNOWN1.toHTML(this));
-            BUILDER.append("</p>");
+            BUILDER.append("<p>Potential match for unknown1 found using general search:<br/>")
+            BUILDER.append(UNKNOWN1.toHTML(this))
+            BUILDER.append("</p>")
         }
-
-        Linkable UNKNOWN2 = save.getPapyrus().getContext().broadSpectrumSearch(this.getData().UNKNOWN2);
+        val UNKNOWN2 = save.papyrus.context.broadSpectrumSearch(data!!.UNKNOWN2)
         if (null != UNKNOWN2) {
-            BUILDER.append("<p>Potential match for unknown2 found using general search:<br/>");
-            BUILDER.append(UNKNOWN2.toHTML(this));
-            BUILDER.append("</p>");
+            BUILDER.append("<p>Potential match for unknown2 found using general search:<br/>")
+            BUILDER.append(UNKNOWN2.toHTML(this))
+            BUILDER.append("</p>")
         }
-
-        save.getPapyrus().printReferrents(this, BUILDER, "reference");
-
-        BUILDER.append("</html>");
-        return BUILDER.toString();
+        save.papyrus.printReferrents(this, BUILDER, "reference")
+        BUILDER.append("</html>")
+        return BUILDER.toString()
     }
+    /**
+     * @return The `ReferenceData` for the instance.
+     */
+    /**
+     * Sets the data field.
+     *
+     * @param newData The new value for the data field.
+     */
+    var data: ReferenceData? = null
 
-    private ReferenceData data;
-
-    final public class ReferenceData implements PapyrusDataFor<Reference> {
+    inner class ReferenceData(input: ByteBuffer, context: PapyrusContext) : PapyrusDataFor<Reference?> {
+        /**
+         * @see resaver.ess.Element.write
+         * @param output The output stream.
+         */
+        override fun write(output: ByteBuffer) {
+            id.write(output)
+            output.put(FLAG)
+            TYPE.write(output)
+            output.putInt(UNKNOWN1)
+            if ((FLAG and 0x04).toInt() != 0) {
+                output.putInt(UNKNOWN2)
+            }
+            output.putInt(VARIABLES!!.size)
+            VARIABLES!!.forEach(Consumer { `var`: Variable -> `var`.write(output) })
+        }
 
         /**
-         * Creates a new <code>ReferenceData</code> by reading from a
-         * <code>LittleEndianDataOutput</code>. No error handling is performed.
+         * @see resaver.ess.Element.calculateSize
+         * @return The size of the `Element` in bytes.
+         */
+        override fun calculateSize(): Int {
+            var sum = 9
+            sum += id.calculateSize()
+            if ((FLAG and 0x04).toInt() != 0) {
+                sum += 4
+            }
+            sum += TYPE.calculateSize()
+            sum += VARIABLES!!.parallelStream().mapToInt { obj: Variable -> obj.calculateSize() }
+                .sum()
+            return sum
+        }
+
+        private val FLAG: Byte
+        val TYPE: TString
+        val UNKNOWN1: Int
+        val UNKNOWN2: Int
+        var VARIABLES: MutableList<Variable>? = null
+
+        /**
+         * Creates a new `ReferenceData` by reading from a
+         * `LittleEndianDataOutput`. No error handling is performed.
          *
          * @param input The input stream.
-         * @param context The <code>PapyrusContext</code> info.
+         * @param context The `PapyrusContext` info.
          * @throws PapyrusElementException
          * @throws PapyrusFormatException
          */
-        public ReferenceData(ByteBuffer input, PapyrusContext context) throws PapyrusElementException, PapyrusFormatException {
-            this.FLAG = input.get();
-            this.TYPE = context.readTString(input);
-            this.UNKNOWN1 = input.getInt();
-            this.UNKNOWN2 = ((this.FLAG & 0x04) != 0 ? input.getInt() : 0);
-
+        init {
+            FLAG = input.get()
+            TYPE = context.readTString(input)
+            UNKNOWN1 = input.int
+            UNKNOWN2 = if ((FLAG and 0x04).toInt() != 0) input.int else 0
             try {
-                int count = input.getInt();
-                this.VARIABLES = Variable.readList(input, count, context);
-            } catch (ListException ex) {
-                throw new PapyrusElementException("Couldn't read struct variables.", ex, this);
+                val count = input.int
+                VARIABLES = Variable.readList(input, count, context)
+            } catch (ex: ListException) {
+                throw PapyrusElementException("Couldn't read struct variables.", ex, this)
             }
         }
-
-        /**
-         * @see resaver.ess.Element#write(ByteBuffer)
-         * @param output The output stream.
-         */
-        @Override
-        public void write(ByteBuffer output) {
-            getID().write(output);
-            output.put(this.FLAG);
-            this.TYPE.write(output);
-            output.putInt(this.UNKNOWN1);
-
-            if ((this.FLAG & 0x04) != 0) {
-                output.putInt(this.UNKNOWN2);
-            }
-
-            output.putInt(this.VARIABLES.size());
-            this.VARIABLES.forEach(var -> var.write(output));
-        }
-
-        /**
-         * @see resaver.ess.Element#calculateSize()
-         * @return The size of the <code>Element</code> in bytes.
-         */
-        @Override
-        public int calculateSize() {
-            int sum = 9;
-            sum += getID().calculateSize();
-            if ((this.FLAG & 0x04) != 0) {
-                sum += 4;
-            }
-            sum += this.TYPE.calculateSize();
-            sum += this.VARIABLES.parallelStream().mapToInt(Variable::calculateSize).sum();
-            return sum;
-        }
-
-        final private byte FLAG;
-        final private TString TYPE;
-        final private int UNKNOWN1;
-        final private int UNKNOWN2;
-        final private List<Variable> VARIABLES;
-
     }
 }
