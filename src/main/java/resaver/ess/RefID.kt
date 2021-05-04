f8ed5a1ed944c6b81bad4d099b008ee0a8356053
noah the goodra <peterpan0413@live.com>
2021-05-04 03:04:35 -0500

    convert Refid

--- src/main/java/resaver/ess/RefID.kt
+++ src/main/java/resaver/ess/RefID.kt
@@ -13,191 +13,177 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package resaver.ess;
+package resaver.ess
 
-import java.nio.ByteBuffer;
-import java.util.Objects;
-import static resaver.ResaverFormatting.zeroPad6;
-import static resaver.ResaverFormatting.zeroPad8;
+import resaver.Analysis
+import resaver.ResaverFormatting
+import java.nio.ByteBuffer
+
 
 /**
  * Describes 3-byte formIDs from Skyrim savegames.
  *
  * @author Mark Fairchild
  */
-final public class RefID implements Element, Linkable, Comparable<RefID> {
-
+class RefID internal constructor(private val DATA: Int, ess: ESS) : Element, Linkable, Comparable<RefID?> {
     /**
-     * Creates a new <code>RefID</code> directly.
-     *
-     * @param newData
-     * @param ess The savefile for context.
-     */
-    RefID(int newData, ESS ess) {
-        this.DATA = newData;
-
-        if (this.isZero()) {
-            this.FORMID = 0;
-            this.PLUGIN = null;
-        } else {
-            final PluginInfo PLUGINS = ess.getPluginInfo();
-
-            switch (this.getType()) {
-                case DEFAULT:
-                    this.PLUGIN = PLUGINS.getFullPlugins().get(0);
-                    this.FORMID = this.getValPart();
-                    break;
-                case CREATED:
-                    this.FORMID = 0xFF000000 | this.getValPart();
-                    this.PLUGIN = null;
-                    break;
-                case FORMIDX:
-                    assert this.getValPart() > 0 : "Invalid form index: " + this.getValPart();
-                    final int FORM_INDEX = this.getValPart() - 1;
-
-                    if (FORM_INDEX < ess.getFormIDs().length) {
-                        this.FORMID = ess.getFormIDs()[FORM_INDEX];
-                        this.PLUGIN = ess.getPluginFor(this.FORMID);
-                    } else {
-                        this.FORMID = -1;
-                        this.PLUGIN = null;
-                    }
-                    break;
-                default:
-                    this.FORMID = 0;
-                    this.PLUGIN = null;
-                    break;
-            }
-        }
-
-        this.name = ess.getAnalysis() == null ? null : ess.getAnalysis().getName(this.PLUGIN, this.FORMID);
-    }
-
-    /**
-     * @see resaver.ess.Element#write(java.nio.ByteBuffer)
+     * @see resaver.ess.Element.write
      * @param output The output stream.
      */
-    @Override
-    public void write(ByteBuffer output) {
-        output.put((byte) (this.DATA >> 16));
-        output.put((byte) (this.DATA >> 8));
-        output.put((byte) (this.DATA >> 0));
+    override fun write(output: ByteBuffer?) {
+        output!!.put((DATA shr 16).toByte())
+        output.put((DATA shr 8).toByte())
+        output.put((DATA shr 0).toByte())
     }
 
     /**
-     * @see resaver.ess.Element#calculateSize()
-     * @return The size of the <code>Element</code> in bytes.
+     * @see resaver.ess.Element.calculateSize
+     * @return The size of the `Element` in bytes.
      */
-    @Override
-    public int calculateSize() {
-        return 3;
+    override fun calculateSize(): Int {
+        return 3
     }
 
     /**
      * @return The type of RefID.
      */
-    public Type getType() {
-        int index = 0x3 & this.DATA >>> 22;
-        switch (index) {
-            case 0:
-                return Type.FORMIDX;
-            case 1:
-                return Type.DEFAULT;
-            case 2:
-                return Type.CREATED;
-            case 3:
-            default:
-                return Type.INVALID;
+    val type: Type
+        get() {
+            return when (0x3 and DATA ushr 22) {
+                0 -> Type.FORMIDX
+                1 -> Type.DEFAULT
+                2 -> Type.CREATED
+                3 -> Type.INVALID
+                else -> Type.INVALID
+            }
         }
-    }
 
     /**
      * @return The value portion of the RefID, which is guaranteed to be non-negative.
      */
-    private int getValPart() {
-        return this.DATA & 0x3FFFFF;
-    }
-
-    /**
-     * @return The name field, if any.
-     */
-    public String getName() {
-        return this.name;
-    }
+    private val valPart: Int
+        private get() = DATA and 0x3FFFFF
 
     /**
      * @return A flag indicating if the RefID is zero.
      */
-    public boolean isZero() {
-        return this.getValPart() == 0;
-    }
+    val isZero: Boolean
+        get() = valPart == 0
 
     /**
      * Adds the EDID/FULL field for the RefID.
      *
      * @param analysis The analysis data.
      */
-    public void addNames(resaver.Analysis analysis) {
-        this.name = analysis.getName(this.PLUGIN, this.FORMID);
+    fun addNames(analysis: Analysis) {
+        name = analysis.getName(PLUGIN, FORMID)
     }
 
     /**
      * @return String representation.
      */
-    @Override
-    public String toString() {
-        if (this.FORMID == 0) {
-            return this.getType() + ":" + zeroPad6(this.DATA);
-        } else if (null != this.name) {
-            return zeroPad8(this.FORMID) + " (" + this.name + ")";
-        } else {
-            return zeroPad8(this.FORMID);
+    override fun toString(): String {
+        return when {
+            FORMID == 0 -> {
+                "$type:${ResaverFormatting.zeroPad6(DATA)}"
+            }
+            null != name -> {
+                "${ResaverFormatting.zeroPad8(FORMID)} ($name)"
+            }
+            else -> {
+                ResaverFormatting.zeroPad8(FORMID)
+            }
         }
     }
 
     /**
-     * @see resaver.ess.Linkable#toHTML(Element)
-     * @param target A target within the <code>Linkable</code>.
+     * @see resaver.ess.Linkable.toHTML
+     * @param target A target within the `Linkable`.
      * @return
      */
-    @Override
-    public String toHTML(Element target) {
-        final String HEX = String.format("%06x", this.DATA);
-        return Linkable.makeLink("refid", HEX, this.toString());
+    override fun toHTML(target: Element): String {
+        val HEX = String.format("%06x", DATA)
+        return Linkable.makeLink("refid", HEX, this.toString())
     }
 
-    @Override
-    public int compareTo(RefID other) {
-        Objects.requireNonNull(other);
-        return Integer.compareUnsigned(this.DATA, other.DATA);
+    override fun compareTo(other: RefID?): Int {
+        return Integer.compareUnsigned(DATA, other?.DATA ?: 0)
     }
 
-    @Override
-    public int hashCode() {
-        return Integer.hashCode(this.DATA);
+    override fun hashCode(): Int {
+        return Integer.hashCode(DATA)
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        } else if (obj == null) {
-            return false;
-        } else {
-            return obj instanceof RefID && ((RefID) obj).DATA == this.DATA;
+    override fun equals(other: Any?): Boolean {
+        return when {
+            this === other -> {
+                true
+            }
+            other == null -> {
+                false
+            }
+            else -> {
+                other is RefID && other.DATA == DATA
+            }
         }
     }
 
-    final private int DATA;
-    final public int FORMID;
-    final public Plugin PLUGIN;
-    private String name;
+    @JvmField
+    var FORMID = 0
+    @JvmField
+    var PLUGIN: Plugin? = null
+
+    /**
+     * @return The name field, if any.
+     */
+    var name: String?
+        private set
 
     /**
      * The four types of RefIDs.
      */
-    public enum Type {
-        FORMIDX, DEFAULT, CREATED, INVALID;
+    enum class Type {
+        FORMIDX, DEFAULT, CREATED, INVALID
     }
 
+    /**
+     * Creates a new `RefID` directly.
+     *
+     * @param newData
+     * @param ess The savefile for context.
+     */
+    init {
+        if (isZero) {
+            FORMID = 0
+            PLUGIN = null
+        } else {
+            val PLUGINS = ess.pluginInfo
+            when (type) {
+                Type.DEFAULT -> {
+                    PLUGIN = PLUGINS.fullPlugins[0]
+                    FORMID = valPart
+                }
+                Type.CREATED -> {
+                    FORMID = -0x1000000 or valPart
+                    PLUGIN = null
+                }
+                Type.FORMIDX -> {
+                    assert(valPart > 0) { "Invalid form index: $valPart" }
+                    val FORM_INDEX = valPart - 1
+                    if (FORM_INDEX < ess.formIDs.size) {
+                        FORMID = ess.formIDs[FORM_INDEX]
+                        PLUGIN = ess.getPluginFor(FORMID)
+                    } else {
+                        FORMID = -1
+                        PLUGIN = null
+                    }
+                }
+                else -> {
+                    FORMID = 0
+                    PLUGIN = null
+                }
+            }
+        }
+        name = if (ess.analysis == null) null else ess.analysis.getName(PLUGIN, FORMID)
+    }
 }
