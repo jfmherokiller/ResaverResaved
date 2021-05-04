4d79bc6ea1efccf68a25643a27993e4f297c518e
noah the goodra <peterpan0413@live.com>
2021-05-04 02:54:58 -0500

    convert plugin

--- src/main/java/resaver/ess/Plugin.kt
+++ src/main/java/resaver/ess/Plugin.kt
@@ -13,124 +13,60 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package resaver.ess;
+package resaver.ess
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import resaver.Analysis;
-import resaver.ess.papyrus.ScriptInstance;
+import mf.BufferUtil
+import resaver.Analysis
+import resaver.ess.papyrus.ScriptInstance
+import java.io.IOException
+import java.nio.ByteBuffer
+import java.nio.charset.StandardCharsets
+import kotlin.streams.toList
 
 /**
  * Abstraction for plugins.
  *
  * @author Mark Fairchild
  */
-final public class Plugin implements Element, AnalyzableElement, Linkable, Comparable<Plugin> {
-
-    static public Plugin PROTOTYPE = new Plugin("Unofficial Skyrim Legendary Edition Patch", -1, false);
-
+class Plugin private constructor(name: String, index: Int, lightweight: Boolean) : Element, AnalyzableElement, Linkable,
+    Comparable<Plugin?> {
     /**
-     * Creates a new <code>Plugin</code> by reading from an input stream.
-     *
-     * @param input The input stream.
-     * @param index The index of the plugin.
-     * @return The new Plugin.
-     */
-    static public Plugin readFullPlugin(ByteBuffer input, int index) {
-        Objects.requireNonNull(input);
-        if (index < 0 || index > 255) {
-            throw new IllegalArgumentException("Invalid index: " + index);
-        }
-
-        String name = mf.BufferUtil.getWString(input);
-        return new Plugin(name, index, false);
-
-    }
-
-    /**
-     * Creates a new <code>Plugin</code> by reading from an input stream.
-     *
-     * @param input The input stream.
-     * @param index The index of the plugin.
-     * @return The new Plugin.
-     * @throws IOException
-     */
-    static public Plugin readLitePlugin(ByteBuffer input, int index) throws IOException {
-        if (index < 0 || index >= 4096) {
-            throw new IllegalArgumentException("Invalid index: " + index);
-        }
-
-        String name = mf.BufferUtil.getWString(input);
-        return new Plugin(name, index, true);
-
-    }
-
-    /**
-     * Creates a new <code>Plugin</code>.
-     *
-     * @param name The name of the plugin.
-     * @param index The index of the plugin.
-     * @param lightweight A flag indicating that it is a lightweight plugin.
-     * @throws IOException
-     */
-    private Plugin(String name, int index, boolean lightweight) {
-        Objects.requireNonNull(name);
-        this.NAME = name;
-        this.INDEX = index;
-        this.LIGHTWEIGHT = lightweight;
-    }
-
-    /**
-     * @see resaver.ess.Element#write(ByteBuffer)
+     * @see resaver.ess.Element.write
      * @param output The output stream.
      */
-    @Override
-    public void write(ByteBuffer output) {
-        mf.BufferUtil.putWString(output, this.NAME);
+    override fun write(output: ByteBuffer?) {
+        BufferUtil.putWString(output, NAME)
     }
 
     /**
-     * @see resaver.ess.Element#calculateSize()
-     * @return The size of the <code>Element</code> in bytes.
+     * @see resaver.ess.Element.calculateSize
+     * @return The size of the `Element` in bytes.
      */
-    @Override
-    public int calculateSize() {
-        return 2 + this.NAME.getBytes(StandardCharsets.ISO_8859_1).length;
+    override fun calculateSize(): Int {
+        return 2 + NAME.toByteArray(StandardCharsets.ISO_8859_1).size
     }
 
     /**
-     * @see resaver.ess.Linkable#toHTML(Element)
-     * @param target A target within the <code>Linkable</code>.
+     * @see resaver.ess.Linkable.toHTML
+     * @param target A target within the `Linkable`.
      * @return
      */
-    @Override
-    public String toHTML(Element target) {
-        return Linkable.makeLink("plugin", this.NAME, this.toString());
+    override fun toHTML(target: Element): String {
+        return Linkable.makeLink("plugin", NAME, this.toString())
     }
 
     /**
      * @return String representation.
      */
-    @Override
-    public String toString() {
-        return this.NAME;
+    override fun toString(): String {
+        return NAME
     }
 
     /**
      * @return String representation.
      */
-    public String indexName() {
-        return this.LIGHTWEIGHT
-                ? String.format("FE%03x: %s", this.INDEX, this.NAME)
-                : String.format("%02x: %s", this.INDEX, this.NAME);
+    fun indexName(): String {
+        return if (LIGHTWEIGHT) String.format("FE%03x: %s", INDEX, NAME) else String.format("%02x: %s", INDEX, NAME)
     }
 
     /**
@@ -139,12 +75,10 @@ final public class Plugin implements Element, AnalyzableElement, Linkable, Compa
      * @param save The savefile to search.
      * @return A set of changeforms.
      */
-    public Set<ChangeForm> getChangeForms(ESS save) {
-        final Set<ChangeForm> FORMS = save.getChangeForms().values().stream()
-                .filter(form -> form.getRefID().PLUGIN == this)
-                .collect(Collectors.toSet());
-
-        return FORMS;
+    fun getChangeForms(save: ESS?): Set<ChangeForm> {
+        val FORMS: Set<ChangeForm> = save!!.changeForms.values.stream()
+            .filter { form: ChangeForm -> form.refID.PLUGIN === this }.toList().toSet()
+        return FORMS
     }
 
     /**
@@ -153,157 +87,186 @@ final public class Plugin implements Element, AnalyzableElement, Linkable, Compa
      * @param save The savefile to search.
      * @return A set of scriptinstances.
      */
-    public Set<ScriptInstance> getInstances(ESS save) {
-        final Set<ScriptInstance> INSTANCES = save.getPapyrus().getScriptInstances().values().stream()
-                .filter(instance -> instance.getRefID() != null)
-                .filter(instance -> instance.getRefID().PLUGIN == this)
-                .collect(Collectors.toSet());
-
-        return INSTANCES;
+    fun getInstances(save: ESS?): Set<ScriptInstance> {
+        val INSTANCES: Set<ScriptInstance> = save!!.papyrus.scriptInstances.values.stream()
+            .filter { instance: ScriptInstance -> instance.refID != null }
+            .filter { instance: ScriptInstance -> instance.refID.PLUGIN === this }.toList().toSet()
+        return INSTANCES
     }
 
     /**
-     * @see AnalyzableElement#getInfo(resaver.Analysis, resaver.ess.ESS)
+     * @see AnalyzableElement.getInfo
      * @param analysis
      * @param save
      * @return
      */
-    @Override
-    public String getInfo(resaver.Analysis analysis, ESS save) {
-        final StringBuilder BUILDER = new StringBuilder();
-
-        if (this.LIGHTWEIGHT) {
-            BUILDER.append("<html><h3>LITE PLUGIN</h3>");
-            BUILDER.append("<p>Name: ").append(this.NAME).append("</p>");
-            BUILDER.append("<p>Index: FE:").append(this.INDEX).append("</p>");
+    override fun getInfo(analysis: Analysis?, save: ESS?): String {
+        val BUILDER: StringBuilder = StringBuilder()
+        if (LIGHTWEIGHT) {
+            BUILDER.append("<html><h3>LITE PLUGIN</h3>")
+            BUILDER.append("<p>Name: ").append(NAME).append("</p>")
+            BUILDER.append("<p>Index: FE:").append(INDEX).append("</p>")
         } else {
-            BUILDER.append("<html><h3>FULL PLUGIN</h3>");
-            BUILDER.append("<p>Name: ").append(this.NAME).append("</p>");
-            BUILDER.append("<p>Index: ").append(this.INDEX).append("</p>");
+            BUILDER.append("<html><h3>FULL PLUGIN</h3>")
+            BUILDER.append("<p>Name: ").append(NAME).append("</p>")
+            BUILDER.append("<p>Index: ").append(INDEX).append("</p>")
         }
-
-        final Set<ChangeForm> FORMS = this.getChangeForms(save);
-        final Set<ScriptInstance> INSTANCES = this.getInstances(save);
-
-        BUILDER.append("<p>").append(FORMS.size()).append(" ChangeForms.</p>");
-        if (FORMS.size() < 100) {
-            BUILDER.append("<ul>");
-            FORMS.forEach(form -> BUILDER.append("<li>").append(form.toHTML(null)));
-            BUILDER.append("</ul>");
+        val FORMS: Set<ChangeForm> = getChangeForms(save)
+        val INSTANCES: Set<ScriptInstance> = getInstances(save)
+        BUILDER.append("<p>").append(FORMS.size).append(" ChangeForms.</p>")
+        if (FORMS.size < 100) {
+            BUILDER.append("<ul>")
+            FORMS.forEach { form: ChangeForm -> BUILDER.append("<li>").append(form.toHTML(null)) }
+            BUILDER.append("</ul>")
         }
-
-        BUILDER.append("<p>").append(INSTANCES.size()).append(" ScriptInstances.</p>");
-        if (INSTANCES.size() < 100) {
-            BUILDER.append("<ul>");
-            INSTANCES.forEach(instance -> BUILDER.append("<li>").append(instance.toHTML(null)));
-            BUILDER.append("</ul>");
+        BUILDER.append("<p>").append(INSTANCES.size).append(" ScriptInstances.</p>")
+        if (INSTANCES.size < 100) {
+            BUILDER.append("<ul>")
+            INSTANCES.forEach { instance: ScriptInstance ->
+                BUILDER.append("<li>").append(instance.toHTML(null))
+            }
+            BUILDER.append("</ul>")
         }
-
         if (null != analysis) {
-            final List<String> PROVIDERS = new ArrayList<>();
-
-            Predicate<String> espFilter = esp -> esp.equalsIgnoreCase(NAME);
-            analysis.ESPS.forEach((mod, esps) -> {
-                esps.stream().filter(espFilter).forEach(esp -> PROVIDERS.add(mod));
-            });
-
-            if (!PROVIDERS.isEmpty()) {
-                String probableProvider = PROVIDERS.get(PROVIDERS.size() - 1);
-
-                Predicate<SortedSet<String>> modFilter = e -> e.contains(probableProvider);
-                int numScripts = (int) analysis.SCRIPT_ORIGINS.values().stream().filter(modFilter).count();
-
-                BUILDER.append(String.format("<p>%d scripts.</p>", numScripts));
-                BUILDER.append(String.format("<p>The plugin probably came from mod \"%s\".</p>", probableProvider));
-
-                if (PROVIDERS.size() > 1) {
-                    BUILDER.append("<p>Full list of providers:</p><ul>");
-                    PROVIDERS.forEach(mod -> BUILDER.append(String.format("<li>%s", mod)));
-                    BUILDER.append("</ul>");
+            val PROVIDERS: MutableList<String> = mutableListOf()
+            val espFilter = { esp: String -> esp.equals(NAME, ignoreCase = true) }
+            analysis.ESPS.forEach { (mod: String, esps: Set<String>) ->
+                esps.sorted().stream().filter(espFilter).forEach { esp: String? -> PROVIDERS.add(mod) }
+            }
+            if (PROVIDERS.isNotEmpty()) {
+                val probableProvider: String = PROVIDERS[PROVIDERS.size - 1]
+                val modFilter = { e: Set<String?> -> e.contains(probableProvider) }
+                val numScripts: Int = analysis.SCRIPT_ORIGINS.values.stream().filter(modFilter).sorted().count().toInt()
+                BUILDER.append(String.format("<p>%d scripts.</p>", numScripts))
+                BUILDER.append(String.format("<p>The plugin probably came from mod \"%s\".</p>", probableProvider))
+                if (PROVIDERS.size > 1) {
+                    BUILDER.append("<p>Full list of providers:</p><ul>")
+                    PROVIDERS.forEach { mod: String? -> BUILDER.append(String.format("<li>%s", mod)) }
+                    BUILDER.append("</ul>")
                 }
             }
         }
-
-        BUILDER.append("</html>");
-        return BUILDER.toString();
+        BUILDER.append("</html>")
+        return BUILDER.toString()
     }
 
     /**
-     * @see AnalyzableElement#matches(resaver.Analysis, resaver.Mod)
+     * @see AnalyzableElement.matches
      * @param analysis
      * @param mod
      * @return
      */
-    @Override
-    public boolean matches(Analysis analysis, String mod) {
-        Objects.requireNonNull(analysis);
-        Objects.requireNonNull(mod);
-
-        Predicate<String> filter = esp -> esp.equalsIgnoreCase(NAME);
-        final List<String> PROVIDERS = new ArrayList<>();
-
-        analysis.ESPS.forEach((m, esps) -> {
-            esps.stream().filter(filter).forEach(esp -> PROVIDERS.add(m));
-        });
-
-        return PROVIDERS.contains(mod);
+    override fun matches(analysis: Analysis?, mod: String?): Boolean {
+        val filter = { esp: String -> esp.equals(NAME, ignoreCase = true) }
+        val PROVIDERS: MutableList<String?> = mutableListOf()
+        analysis!!.ESPS.forEach { (m: String?, esps: Set<String>) ->
+            esps.sorted().stream().filter(filter).forEach { esp: String? -> PROVIDERS.add(m) }
+        }
+        return PROVIDERS.contains(mod)
     }
 
     /**
      * The name field.
      */
-    final public String NAME;
+    @JvmField
+    val NAME: String = name
 
     /**
      * The index field.
      */
-    final public int INDEX;
+    @JvmField
+    val INDEX: Int = index
 
     /**
      *
      */
-    final public boolean LIGHTWEIGHT;
+    @JvmField
+    val LIGHTWEIGHT: Boolean = lightweight
 
     /**
-     * @see Comparable#compareTo(java.lang.Object)
-     * @param o
+     * @see Comparable.compareTo
+     * @param other
      * @return
      */
-    @Override
-    public int compareTo(Plugin o) {
-        if (null == o) {
-            return 1;
+    override fun compareTo(other: Plugin?): Int {
+        if (null == other) {
+            return 1
+        }
+        return java.util.Objects.compare(NAME, other.NAME) { obj: String, str: String? ->
+            obj.compareTo(
+                (str)!!, ignoreCase = true
+            )
+        }
+    }
+
+    /**
+     * @see Object.hashCode
+     * @return
+     */
+    override fun hashCode(): Int {
+        var hash = 3
+        hash = 29 * hash + (NAME.toLowerCase()).hashCode()
+        return hash
+    }
+
+    /**
+     * @see Object.equals
+     * @return
+     */
+    override fun equals(other: Any?): Boolean {
+        return when {
+            this === other -> {
+                true
+            }
+            other == null -> {
+                false
+            }
+            javaClass != other.javaClass -> {
+                false
+            }
+            else -> {
+                val other2: Plugin = other as Plugin
+                NAME.equals(other2.NAME, ignoreCase = true)
+            }
+        }
+    }
+
+    companion object {
+        @JvmField
+        var PROTOTYPE: Plugin = Plugin("Unofficial Skyrim Legendary Edition Patch", -1, false)
+
+        /**
+         * Creates a new `Plugin` by reading from an input stream.
+         *
+         * @param input The input stream.
+         * @param index The index of the plugin.
+         * @return The new Plugin.
+         */
+        @JvmStatic
+        fun readFullPlugin(input: ByteBuffer?, index: Int): Plugin {
+            if (index < 0 || index > 255) {
+                throw IllegalArgumentException("Invalid index: $index")
+            }
+            val name: String = BufferUtil.getWString(input)
+            return Plugin(name, index, false)
         }
 
-        return Objects.compare(this.NAME, o.NAME, String::compareToIgnoreCase);
-    }
-
-    /**
-     * @see Object#hashCode()
-     * @return
-     */
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 29 * hash + Objects.hashCode(this.NAME.toLowerCase());
-        return hash;
-    }
-
-    /**
-     * @see Object#equals(java.lang.Object)
-     * @return
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        } else if (obj == null) {
-            return false;
-        } else if (getClass() != obj.getClass()) {
-            return false;
-        } else {
-            final Plugin other = (Plugin) obj;
-            return this.NAME.equalsIgnoreCase(other.NAME);
+        /**
+         * Creates a new `Plugin` by reading from an input stream.
+         *
+         * @param input The input stream.
+         * @param index The index of the plugin.
+         * @return The new Plugin.
+         * @throws IOException
+         */
+        @JvmStatic
+        @Throws(IOException::class)
+        fun readLitePlugin(input: ByteBuffer?, index: Int): Plugin {
+            if (index < 0 || index >= 4096) {
+                throw IllegalArgumentException("Invalid index: $index")
+            }
+            val name: String = BufferUtil.getWString(input)
+            return Plugin(name, index, true)
         }
     }
 
