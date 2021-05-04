18cfc32f994900245722c0bf34049e67ba96c4ea
noah the goodra <peterpan0413@live.com>
2021-05-03 21:49:58 -0500

    counter to kotlin

--- src/main/java/mf/Counter.kt
+++ src/main/java/mf/Counter.kt
@@ -13,42 +13,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package mf;
+package mf
+
+import java.util.function.Consumer
+import java.util.function.DoubleConsumer
+import java.util.function.IntConsumer
 
 /**
  * A class for counting.
  *
  * @author Mark Fairchild
  */
-final public class Counter {
-
-    /**
-     * Create a new <code>Counter</code> with the specified limit.
-     *
-     * @param newLimit The limit for the counter. Must be non-negative.
-     */
-    public Counter(int newLimit) {
-        if (newLimit < 0) {
-            throw new IllegalArgumentException("Limit must be non-negative: " + newLimit);
-        }
-
-        this.reset(newLimit);       
-        this.percentListeners = null;
-        this.countListeners = null;
-    }
-
+class Counter(newLimit: Int) {
     /**
      * Increases the count by one. Any listeners will be notified.
      */
-    public void click() {
-        this.value++;
-
-        if (this.percentListeners != null && !this.percentListeners.isEmpty()) {
-            double percent = (double) this.value / (double) this.limit;
-            this.percentListeners.forEach(l -> l.accept(percent));
+    fun click() {
+        counter++
+        if (percentListeners != null && percentListeners!!.isNotEmpty()) {
+            val percent = counter.toDouble() / limit.toDouble()
+            percentListeners!!.forEach(Consumer { l: DoubleConsumer -> l.accept(percent) })
         }
-        if (this.countListeners != null && !this.countListeners.isEmpty()) {
-            this.countListeners.forEach(l -> l.accept(this.value));
+        if (countListeners != null && countListeners!!.isNotEmpty()) {
+            countListeners!!.forEach(Consumer { l: IntConsumer -> l.accept(counter) })
         }
     }
 
@@ -58,19 +45,15 @@ final public class Counter {
      *
      * @param num The number of clicks. Must be non-negative.
      */
-    public void click(int num) {
-        if (num < 0) {
-            throw new IllegalArgumentException("Clicks must be non-negative: " + num);
+    fun click(num: Int) {
+        require(num >= 0) { "Clicks must be non-negative: $num" }
+        counter += num
+        if (percentListeners != null && percentListeners!!.isNotEmpty()) {
+            val percent = percentage
+            percentListeners!!.forEach(Consumer { l: DoubleConsumer -> l.accept(percent) })
         }
-
-        this.value += num;
-
-        if (this.percentListeners != null && !this.percentListeners.isEmpty()) {
-            double percent = this.getPercentage();
-            this.percentListeners.forEach(l -> l.accept(percent));
-        }
-        if (this.countListeners != null && !this.countListeners.isEmpty()) {
-            this.countListeners.forEach(l -> l.accept(this.value));
+        if (countListeners != null && countListeners!!.isNotEmpty()) {
+            countListeners!!.forEach(Consumer { l: IntConsumer -> l.accept(counter) })
         }
     }
 
@@ -79,12 +62,9 @@ final public class Counter {
      *
      * @throws IllegalStateException Thrown if the count would become negative.
      */
-    public void unclick() {
-        if (this.value == 0) {
-            throw new IllegalStateException("Counter can't be negative.");
-        }
-
-        this.value--;
+    fun unclick() {
+        check(counter != 0) { "Counter can't be negative." }
+        counter--
     }
 
     /**
@@ -94,14 +74,10 @@ final public class Counter {
      * @param num The number of clicks. Must be non-negative.
      * @throws IllegalStateException Thrown if the count would become negative.
      */
-    public void unclick(int num) {
-        if (num < 0) {
-            throw new IllegalArgumentException("Clicks must be non-negative: " + num);
-        } else if (this.value < num) {
-            throw new IllegalStateException("Counter can't be negative.");
-        }
-
-        this.value -= num;
+    fun unclick(num: Int) {
+        require(num >= 0) { "Clicks must be non-negative: $num" }
+        check(counter >= num) { "Counter can't be negative." }
+        counter -= num
     }
 
     /**
@@ -109,15 +85,12 @@ final public class Counter {
      *
      * @param newLimit The new limit. Must be non-negative.
      */
-    public void reset(int newLimit) {
-        if (newLimit < 0) {
-            throw new IllegalArgumentException("Limit must be non-negative: " + newLimit);
-        }
-        
-        this.value = 0;
-        this.limit = newLimit;        
-        int width = Integer.toString(this.limit).length();
-        this.format = new StringBuilder().append('%').append(width).append("d/%").append(width).append('d').toString();
+    fun reset(newLimit: Int) {
+        require(newLimit >= 0) { "Limit must be non-negative: $newLimit" }
+        counter = 0
+        limit = newLimit
+        val width = limit.toString().length
+        format = "%${width}d/%${width}d"
     }
 
     /**
@@ -125,31 +98,22 @@ final public class Counter {
      *
      * @return The string representation.
      */
-    public String eval() {
-        this.click();
-        return this.toString();
-    }
-
-    /**
-     * @return The current count.
-     */
-    public int getCounter() {
-        return this.value;
+    fun eval(): String {
+        this.click()
+        return this.toString()
     }
 
     /**
      * @return The current count as a percentage of the limit.
      */
-    public double getPercentage() {
-        return (double) this.value / (double) this.limit;
-    }
+    val percentage: Double
+        get() = counter.toDouble() / limit.toDouble()
 
     /**
-     * @return A string representation of the form <code>val/limit</code>.
+     * @return A string representation of the form `val/limit`.
      */
-    @Override
-    public String toString() {
-        return String.format(this.format, this.value, this.limit);
+    override fun toString(): String {
+        return String.format(format!!, counter, limit)
     }
 
     /**
@@ -158,11 +122,11 @@ final public class Counter {
      *
      * @param listener The listener.
      */
-    public void addCountListener(java.util.function.IntConsumer listener) {
-        if (this.countListeners == null) {
-            this.countListeners = new java.util.LinkedList<>();
+    fun addCountListener(listener: IntConsumer) {
+        if (countListeners == null) {
+            countListeners = mutableListOf()
         }
-        this.countListeners.add(listener);
+        countListeners!!.add(listener)
     }
 
     /**
@@ -171,20 +135,20 @@ final public class Counter {
      *
      * @param listener The listener.
      */
-    public void addPercentListener(java.util.function.DoubleConsumer listener) {
-        if (this.percentListeners == null) {
-            this.percentListeners = new java.util.LinkedList<>();
+    fun addPercentListener(listener: DoubleConsumer) {
+        if (percentListeners == null) {
+            percentListeners = mutableListOf()
         }
-        this.percentListeners.add(listener);
+        percentListeners!!.add(listener)
     }
 
     /**
      * Removes a listener.
      * @param listener The listener.
      */
-    public void removeCountListener(java.util.function.IntConsumer listener) {
-        if (this.countListeners != null) {
-            this.countListeners.remove(listener);
+    fun removeCountListener(listener: IntConsumer) {
+        if (countListeners != null) {
+            countListeners!!.remove(listener)
         }
     }
 
@@ -192,16 +156,31 @@ final public class Counter {
      * Removes a listener.
      * @param listener The listener.
      */
-    public void removePercentListener(java.util.function.DoubleConsumer listener) {
-        if (this.percentListeners != null) {
-            this.percentListeners.remove(listener);
+    fun removePercentListener(listener: DoubleConsumer) {
+        if (percentListeners != null) {
+            percentListeners!!.remove(listener)
         }
     }
 
-    private int value;
-    private int limit;
-    private String format;
-    private java.util.List<java.util.function.DoubleConsumer> percentListeners;
-    private java.util.List<java.util.function.IntConsumer> countListeners;
+    /**
+     * @return The current count.
+     */
+    var counter = 0
+        private set
+    private var limit = 0
+    private var format: String? = null
+    private var percentListeners: MutableList<DoubleConsumer>?
+    private var countListeners: MutableList<IntConsumer>?
 
+    /**
+     * Create a new `Counter` with the specified limit.
+     *
+     * @param newLimit The limit for the counter. Must be non-negative.
+     */
+    init {
+        require(newLimit >= 0) { "Limit must be non-negative: $newLimit" }
+        reset(newLimit)
+        percentListeners = null
+        countListeners = null
+    }
 }
