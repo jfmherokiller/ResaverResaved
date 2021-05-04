1a13658fbe5cfca96153c877a8139b478d9d8727
noah the goodra <peterpan0413@live.com>
2021-05-04 03:11:09 -0500

    convert globalvariable

--- src/main/java/resaver/ess/GlobalVariable.kt
+++ src/main/java/resaver/ess/GlobalVariable.kt
@@ -13,117 +13,96 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package resaver.ess;
+package resaver.ess
 
-import java.nio.ByteBuffer;
-import java.util.Objects;
-import resaver.ess.papyrus.EID;
+import resaver.ess.ESS.ESSContext
+import resaver.ess.papyrus.EID
+import java.nio.ByteBuffer
 
 /**
  *
  * @author Mark
  */
-public class GlobalVariable implements Element {
-
+class GlobalVariable(input: ByteBuffer?, context: ESSContext) : Element {
     /**
-     * Creates a new <code>GlobalVariable</code>.
-     *
-     * @param input The input stream.
-     * @param context The <code>ESSContext</code> info.
-     */
-    public GlobalVariable(ByteBuffer input, ESS.ESSContext context) {
-        Objects.requireNonNull(input);
-        this.REFID = context.readRefID(input);
-        this.value = input.getFloat();
-    }
-
-    /**
-     * @see resaver.ess.Element#write(java.nio.ByteBuffer) 
+     * @see resaver.ess.Element.write
      * @param output The output stream.
      */
-    @Override
-    public void write(ByteBuffer output) {
-        this.REFID.write(output);
-        output.putFloat(this.value);
+    override fun write(output: ByteBuffer?) {
+        REFID.write(output)
+        output!!.putFloat(value)
     }
 
     /**
-     * @see resaver.ess.Element#calculateSize()
-     * @return The size of the <code>Element</code> in bytes.
+     * @see resaver.ess.Element.calculateSize
+     * @return The size of the `Element` in bytes.
      */
-    @Override
-    public int calculateSize() {
-        return 4 + this.REFID.calculateSize();
+    override fun calculateSize(): Int {
+        return 4 + REFID.calculateSize()
     }
 
-    @Override
-    public String toString() {
-        final StringBuilder BUF = new StringBuilder();
-
-        if (this.REFID.getName() != null) {
-            BUF.append(this.REFID.toString());
+    override fun toString(): String {
+        val BUF = StringBuilder()
+        if (REFID.name != null) {
+            BUF.append(REFID.toString())
         } else {
-            BUF.append("NAME MISSING");
+            BUF.append("NAME MISSING")
         }
-
-        BUF.append(" (");
-
-        if (this.REFID.PLUGIN != null) {
-            BUF.append(this.REFID.PLUGIN.NAME);
-            BUF.append(":");
+        BUF.append(" (")
+        if (REFID.PLUGIN != null) {
+            BUF.append(REFID.PLUGIN!!.NAME)
+            BUF.append(":")
         }
-
-        if (this.REFID.getType() == RefID.Type.FORMIDX) {
-            BUF.append(EID.pad8(this.REFID.FORMID));
+        if (REFID.type === RefID.Type.FORMIDX) {
+            BUF.append(EID.pad8(REFID.FORMID))
         } else {
-            BUF.append(this.REFID.toString());
+            BUF.append(REFID.toString())
         }
-
-        BUF.append(") = ");
-        BUF.append(this.value);
-        return BUF.toString();
+        BUF.append(") = ")
+        BUF.append(value)
+        return BUF.toString()
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 53 * hash + Objects.hashCode(this.REFID);
-        hash = 53 * hash + Float.floatToIntBits(this.value);
-        return hash;
+    override fun hashCode(): Int {
+        var hash = 7
+        hash = 53 * hash + REFID.hashCode()
+        hash = 53 * hash + java.lang.Float.floatToIntBits(value)
+        return hash
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
+            return true
         }
-        if (obj == null) {
-            return false;
+        if (other == null) {
+            return false
         }
-        if (getClass() != obj.getClass()) {
-            return false;
+        if (javaClass != other.javaClass) {
+            return false
         }
-        final GlobalVariable other = (GlobalVariable) obj;
-        return other.REFID.equals(this.REFID) && other.value == this.value;
+        val other2 = other as GlobalVariable
+        return other2.REFID == REFID && other2.value == value
     }
 
+    private val REFID: RefID
     /**
      *
-     * @return The value of the <code>GlobalVariable</code>.
+     * @return The value of the `GlobalVariable`.
      */
-    public float getValue() {
-        return value;
-    }
+    /**
+     * Sets the value of the `GlobalVariable`.
+     * @param newVal The new value of the `GlobalVariable`.
+     */
+    var value: Float
 
     /**
-     * Sets the value of the <code>GlobalVariable</code>.
-     * @param newVal The new value of the <code>GlobalVariable</code>.
+     * Creates a new `GlobalVariable`.
+     *
+     * @param input The input stream.
+     * @param context The `ESSContext` info.
      */
-    public void setValue(float newVal) {
-        this.value = newVal;
+    init {
+        REFID = context.readRefID(input!!)
+        value = input.float ?: 0.0f
     }
-
-    final private RefID REFID;
-    private float value;
-
 }
