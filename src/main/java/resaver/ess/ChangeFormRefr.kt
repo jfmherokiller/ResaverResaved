7a3a9647114747bf8a7ec378ae40db324a2b28f1
noah the goodra <peterpan0413@live.com>
2021-05-06 12:57:09 -0500

    ChangeFormRefr conversion

--- src/main/java/resaver/ess/ChangeFormRefr.kt
+++ src/main/java/resaver/ess/ChangeFormRefr.kt
@@ -13,149 +13,153 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package resaver.ess;
+package resaver.ess
 
-import java.nio.ByteBuffer;
-import java.util.Objects;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import resaver.Analysis;
-import static resaver.ess.ChangeFlagConstantsRef.*;
+import resaver.Analysis
+import resaver.ess.ESS.ESSContext
+import java.nio.ByteBuffer
+import java.util.*
+import java.util.logging.Level
+import java.util.logging.Logger
 
 /**
  * Describes a ChangeForm containing a placed Reference.
  *
  * @author Mark Fairchild
  */
-public class ChangeFormRefr extends GeneralElement implements ChangeFormData {
+class ChangeFormRefr(input: ByteBuffer, flags: Flags.Int, refid: RefID, analysis: Analysis?, context: ESSContext?) :
+    GeneralElement(), ChangeFormData {
+    /**
+     * @see AnalyzableElement.getInfo
+     * @param analysis
+     * @param save
+     * @return
+     */
+    override fun getInfo(analysis: Analysis?, save: ESS?): String {
+        val BUILDER = StringBuilder()
+        if (FLAGS.FLAGS != 0) {
+            BUILDER.append("<hr/>")
+            BUILDER.append("<h3>ChangeFlags</h3><ul>")
+            for (flag in ChangeFlagConstantsRef.values()) {
+                if (FLAGS.getFlag(flag)) {
+                    BUILDER.append("<li>")
+                    BUILDER.append(flag.position)
+                    BUILDER.append(": ")
+                    BUILDER.append(flag)
+                    BUILDER.append("</li><br/>")
+                }
+            }
+            BUILDER.append("</ul>")
+        }
+        BUILDER.append("<hr/>")
+        BUILDER.append("<pre><code>")
+        BUILDER.append(super.toString("REFR", 0))
+        BUILDER.append("</code></pre>")
+        return BUILDER.toString()
+    }
 
     /**
-     * Creates a new <code>ChangeFormRefr</code> by reading from a
-     * <code>LittleEndianDataOutput</code>. No error handling is performed.
+     * @see AnalyzableElement.matches
+     * @param analysis
+     * @param mod
+     * @return
+     */
+    override fun matches(analysis: Analysis?, mod: String?): Boolean {
+        return false
+    }
+
+    // The change flags.
+    private val FLAGS: Flags.Int
+
+    companion object {
+        val LOG = Logger.getLogger(ChangeFormRefr::class.java.canonicalName)
+    }
+
+    /**
+     * Creates a new `ChangeFormRefr` by reading from a
+     * `LittleEndianDataOutput`. No error handling is performed.
      *
      * @param input The input stream.
      * @param flags The ChangeForm flags.
      * @param refid The ChangeForm refid.
      * @param analysis
-     * @param context The <code>ESSContext</code> info.
+     * @param context The `ESSContext` info.
      */
-    public ChangeFormRefr(ByteBuffer input, Flags.Int flags, RefID refid, resaver.Analysis analysis, ESS.ESSContext context) {
-        Objects.requireNonNull(input);
-        this.FLAGS = Objects.requireNonNull(flags);
-
-        int initialType;
-
-        if (refid.getType() == RefID.Type.CREATED) {
-            initialType = 5;
-        } else if (flags.getFlag(CHANGE_REFR_PROMOTED) || flags.getFlag(CHANGE_REFR_CELL_CHANGED)) {
-            initialType = 6;
-        } else if (flags.getFlag(CHANGE_REFR_HAVOK_MOVE) || flags.getFlag(CHANGE_REFR_MOVE)) {
-            initialType = 4;
+    init {
+        Objects.requireNonNull(input)
+        FLAGS = Objects.requireNonNull(flags)
+        val initialType: Int
+        initialType = if (refid.type === RefID.Type.CREATED) {
+            5
+        } else if (flags.getFlag(ChangeFlagConstantsRef.CHANGE_REFR_PROMOTED) || flags.getFlag(ChangeFlagConstantsRef.CHANGE_REFR_CELL_CHANGED)) {
+            6
+        } else if (flags.getFlag(ChangeFlagConstantsRef.CHANGE_REFR_HAVOK_MOVE) || flags.getFlag(ChangeFlagConstantsRef.CHANGE_REFR_MOVE)) {
+            4
         } else {
-            initialType = 0;
+            0
         }
-
         try {
-            super.readElement(input, "INITIAL", in -> new ChangeFormInitialData(in, initialType, context));
-
-            if (flags.getFlag(CHANGE_REFR_HAVOK_MOVE)) {
-                super.readBytesVS(input, "HAVOK");
+            super.readElement(input, "INITIAL") { `in`: ByteBuffer? ->
+                ChangeFormInitialData(
+                    `in`!!, initialType, context
+                )
             }
-
-            if (flags.getFlag(CHANGE_FORM_FLAGS)) {
-                super.readElement(input, CHANGE_FORM_FLAGS, ChangeFormFlags::new);
+            if (flags.getFlag(ChangeFlagConstantsRef.CHANGE_REFR_HAVOK_MOVE)) {
+                super.readBytesVS(input, "HAVOK")
             }
-
-            if (flags.getFlag(CHANGE_REFR_BASEOBJECT)) {
-                super.readRefID(input, "BASE_OBJECT", context);
-            }
-
-            if (flags.getFlag(CHANGE_REFR_SCALE)) {
-                super.readFloat(input, "SCALE");
-            }
-
-            if (flags.getFlag(CHANGE_REFR_MOVE)) {
-                super.readRefID(input, "MOVE_CELL", context);
-                super.readFloats(input, "MOVE_POS", 3);
-                super.readFloats(input, "MOVE_ROT", 3);
-            }
-
-            if (flags.getFlag(CHANGE_REFR_EXTRA_OWNERSHIP)
-                    || flags.getFlag(CHANGE_OBJECT_EXTRA_LOCK)
-                    || flags.getFlag(CHANGE_REFR_EXTRA_ENCOUNTER_ZONE)
-                    || flags.getFlag(CHANGE_REFR_EXTRA_GAME_ONLY)
-                    || flags.getFlag(CHANGE_OBJECT_EXTRA_AMMO)
-                    || flags.getFlag(CHANGE_DOOR_EXTRA_TELEPORT)
-                    || flags.getFlag(CHANGE_REFR_PROMOTED)
-                    || flags.getFlag(CHANGE_REFR_EXTRA_ACTIVATING_CHILDREN)
-                    || flags.getFlag(CHANGE_OBJECT_EXTRA_ITEM_DATA)) {
-                super.readElement(input, "EXTRADATA", in -> new ChangeFormExtraData(in, context));
-            }
-
-            if (flags.getFlag(CHANGE_REFR_INVENTORY) || flags.getFlag(CHANGE_REFR_LEVELED_INVENTORY)) {
-                super.readVSElemArray(input, "INVENTORY", in -> new ChangeFormInventoryItem(in, context));
-            }
-
-            if (flags.getFlag(CHANGE_REFR_ANIMATION)) {
-                super.readBytesVS(input, "ANIMATIONS");
-            }
-
-        } catch (Throwable ex) {
-            LOG.log(Level.WARNING, "Error parsing NPC_ ChangeForm.", ex);
-
-            int count = 0;
-            while (input.hasRemaining()) {
-                int size = Math.min(256, input.capacity() - input.position());
-                super.readBytes(input, "UNPARSED_DATA_" + count, size);
-                count++;
-            }
-        }
-    }
-
-    /**
-     * @see AnalyzableElement#getInfo(resaver.Analysis, resaver.ess.ESS)
-     * @param analysis
-     * @param save
-     * @return
-     */
-    @Override
-    public String getInfo(resaver.Analysis analysis, ESS save) {
-        final StringBuilder BUILDER = new StringBuilder();
-        if (this.FLAGS.FLAGS != 0) {
-            BUILDER.append("<hr/>");
-            BUILDER.append("<h3>ChangeFlags</h3><ul>");
-            for (ChangeFlagConstants flag : ChangeFlagConstantsRef.values()) {
-                if (this.FLAGS.getFlag(flag)) {
-                    BUILDER.append("<li>");
-                    BUILDER.append(flag.getPosition());
-                    BUILDER.append(": ");
-                    BUILDER.append(flag);
-                    BUILDER.append("</li><br/>");
+            if (flags.getFlag(ChangeFlagConstantsRef.CHANGE_FORM_FLAGS)) {
+                super.readElement(input, ChangeFlagConstantsRef.CHANGE_FORM_FLAGS) { input: ByteBuffer? ->
+                    ChangeFormFlags(
+                        input!!
+                    )
                 }
             }
-            BUILDER.append("</ul>");
+            if (flags.getFlag(ChangeFlagConstantsRef.CHANGE_REFR_BASEOBJECT)) {
+                super.readRefID(input, "BASE_OBJECT", context)
+            }
+            if (flags.getFlag(ChangeFlagConstantsRef.CHANGE_REFR_SCALE)) {
+                super.readFloat(input, "SCALE")
+            }
+            if (flags.getFlag(ChangeFlagConstantsRef.CHANGE_REFR_MOVE)) {
+                super.readRefID(input, "MOVE_CELL", context)
+                super.readFloats(input, "MOVE_POS", 3)
+                super.readFloats(input, "MOVE_ROT", 3)
+            }
+            if (flags.getFlag(ChangeFlagConstantsRef.CHANGE_REFR_EXTRA_OWNERSHIP)
+                || flags.getFlag(ChangeFlagConstantsRef.CHANGE_OBJECT_EXTRA_LOCK)
+                || flags.getFlag(ChangeFlagConstantsRef.CHANGE_REFR_EXTRA_ENCOUNTER_ZONE)
+                || flags.getFlag(ChangeFlagConstantsRef.CHANGE_REFR_EXTRA_GAME_ONLY)
+                || flags.getFlag(ChangeFlagConstantsRef.CHANGE_OBJECT_EXTRA_AMMO)
+                || flags.getFlag(ChangeFlagConstantsRef.CHANGE_DOOR_EXTRA_TELEPORT)
+                || flags.getFlag(ChangeFlagConstantsRef.CHANGE_REFR_PROMOTED)
+                || flags.getFlag(ChangeFlagConstantsRef.CHANGE_REFR_EXTRA_ACTIVATING_CHILDREN)
+                || flags.getFlag(ChangeFlagConstantsRef.CHANGE_OBJECT_EXTRA_ITEM_DATA)
+            ) {
+                super.readElement(input, "EXTRADATA") { `in`: ByteBuffer? ->
+                    ChangeFormExtraData(
+                        `in`!!, context!!
+                    )
+                }
+            }
+            if (flags.getFlag(ChangeFlagConstantsRef.CHANGE_REFR_INVENTORY) || flags.getFlag(ChangeFlagConstantsRef.CHANGE_REFR_LEVELED_INVENTORY)) {
+                super.readVSElemArray(input, "INVENTORY") { `in`: ByteBuffer? ->
+                    ChangeFormInventoryItem(
+                        `in`,
+                        context
+                    )
+                }
+            }
+            if (flags.getFlag(ChangeFlagConstantsRef.CHANGE_REFR_ANIMATION)) {
+                super.readBytesVS(input, "ANIMATIONS")
+            }
+        } catch (ex: Throwable) {
+            LOG.log(Level.WARNING, "Error parsing NPC_ ChangeForm.", ex)
+            var count = 0
+            while (input.hasRemaining()) {
+                val size = Math.min(256, input.capacity() - input.position())
+                super.readBytes(input, "UNPARSED_DATA_$count", size)
+                count++
+            }
         }
-
-        BUILDER.append("<hr/>");
-        BUILDER.append("<pre><code>");
-        BUILDER.append(super.toString("REFR", 0));
-        BUILDER.append("</code></pre>");
-        return BUILDER.toString();
     }
-
-    /**
-     * @see AnalyzableElement#matches(resaver.Analysis, java.lang.String) 
-     * @param analysis
-     * @param mod
-     * @return
-     */
-    @Override
-    public boolean matches(Analysis analysis, String mod) {
-        return false;
-    }
-
-    // The change flags.
-    final private Flags.Int FLAGS;
-    static final Logger LOG = Logger.getLogger(ChangeFormRefr.class.getCanonicalName());
-
 }
