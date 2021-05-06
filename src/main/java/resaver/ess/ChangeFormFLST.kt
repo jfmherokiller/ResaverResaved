6eaec6e86f2dde68db62e3970a6104de256c3a7c
noah the goodra <peterpan0413@live.com>
2021-05-06 13:00:23 -0500

    ChangeFormFLST conversion

--- src/main/java/resaver/ess/ChangeFormFLST.kt
+++ src/main/java/resaver/ess/ChangeFormFLST.kt
@@ -13,97 +13,47 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package resaver.ess;
+package resaver.ess
 
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import resaver.Analysis;
+import resaver.Analysis
+import resaver.ess.ESS.ESSContext
+import java.nio.ByteBuffer
+import java.util.*
+
 
 /**
  * Describes a ChangeForm containing a formlist.
  *
  * @author Mark Fairchild
  */
-final public class ChangeFormFLST implements ChangeFormData {
-
+class ChangeFormFLST(input: ByteBuffer, flags: Flags.Int, context: ESSContext) : ChangeFormData {
     /**
-     * Creates a new <code>ChangeForm</code> by reading from a
-     * <code>LittleEndianDataOutput</code>. No error handling is performed.
-     *
-     * @param input The input stream.
-     * @param flags The change flags.
-     * @param context The <code>ESSContext</code> info.
-     */
-    public ChangeFormFLST(ByteBuffer input, Flags.Int flags, ESS.ESSContext context) {
-        Objects.requireNonNull(input);
-
-        this.FLAGS = flags.getFlag(1) ? new ChangeFormFlags(input) : null;
-
-        if (flags.getFlag(31)) {
-            int formCount = input.getInt();
-            if (formCount > 0x3FFF) {
-                throw new IllegalArgumentException("Invalid data: found " + formCount + " formCount in FLST.");
-            }
-
-            this.FORMS = new ArrayList<>(formCount);
-
-            for (int i = 0; i < formCount; i++) {
-                final RefID REF = context.readRefID(input);
-                this.FORMS.add(REF);
-            }
-        } else {
-            this.FORMS = null;
-        }
-    }
-
-    /**
-     * @see resaver.ess.Element#write(java.nio.ByteBuffer)
+     * @see resaver.ess.Element.write
      * @param output The output stream.
      */
-    @Override
-    public void write(ByteBuffer output) {
-        if (null != this.FLAGS) {
-            this.FLAGS.write(output);
-        }
-
-        if (null != this.FORMS) {
-            output.putInt(this.FORMS.size());
-            this.FORMS.forEach(ref -> ref.write(output));
+    override fun write(output: ByteBuffer?) {
+        refID?.write(output)
+        if (null != FORMS) {
+            output!!.putInt(FORMS!!.size)
+            FORMS!!.forEach { ref: RefID -> ref.write(output) }
         }
     }
 
     /**
-     * @see resaver.ess.Element#calculateSize()
-     * @return The size of the <code>Element</code> in bytes.
+     * @see resaver.ess.Element.calculateSize
+     * @return The size of the `Element` in bytes.
      */
-    @Override
-    public int calculateSize() {
-        int sum = 0;
-
-        if (null != this.FLAGS) {
-            sum += this.FLAGS.calculateSize();
+    override fun calculateSize(): Int {
+        var sum = 0
+        if (null != refID) {
+            sum += refID.calculateSize()
         }
-
-        if (null != this.FORMS) {
-            sum += 4;
-            int result = 0;
-            for (RefID FORM : this.FORMS) {
-                int calculateSize = FORM.calculateSize();
-                result += calculateSize;
-            }
-            sum += result;
+        if (null != FORMS) {
+            sum += 4
+            val result = FORMS!!.sumOf { it.calculateSize() }
+            sum += result
         }
-
-        return sum;
-    }
-
-    /**
-     * @return The <code>ChangeFormFlags</code> field.
-     */
-    public ChangeFormFlags getRefID() {
-        return this.FLAGS;
+        return sum
     }
 
     /**
@@ -111,117 +61,138 @@ final public class ChangeFormFLST implements ChangeFormData {
      *
      * @return The number of entries removed.
      */
-    public int cleanse() {
-        if (null == this.FORMS) {
-            return 0;
+    fun cleanse(): Int {
+        if (null == FORMS) {
+            return 0
         }
-
-        int size = this.FORMS.size();
-        this.FORMS.removeIf(RefID::isZero);
-        return size - this.FORMS.size();
+        val size = FORMS!!.size
+        FORMS!!.removeIf(RefID::isZero)
+        return size - FORMS!!.size
     }
 
     /**
      * @return A flag indicating that the formlist has nullref entries.
      */
-    public boolean containsNullrefs() {
-        for (RefID FORM : this.FORMS) {
-            if (FORM.isZero()) {
-                return true;
+    fun containsNullrefs(): Boolean {
+        for (FORM in FORMS!!) {
+            if (FORM.isZero) {
+                return true
             }
         }
-        return false;
+        return false
     }
 
     /**
      * @return String representation.
      */
-    @Override
-    public String toString() {
-        if (null == this.FORMS) {
-            return "";
-
-        } else if (this.containsNullrefs()) {
-            return "(" + this.FORMS.size() + " refs, contains nullrefs)";
-
-        } else {
-            return "(" + this.FORMS.size() + " refs)";
+    override fun toString(): String {
+        return when {
+            null == FORMS -> {
+                ""
+            }
+            containsNullrefs() -> {
+                "(${FORMS!!.size} refs, contains nullrefs)"
+            }
+            else -> {
+                "(${FORMS!!.size} refs)"
+            }
         }
     }
 
     /**
-     * @see Object#hashCode()
+     * @see Object.hashCode
      * @return
      */
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 41 * hash + Objects.hashCode(this.FLAGS);
-        hash = 41 * hash + Objects.hashCode(this.FORMS);
-        return hash;
+    override fun hashCode(): Int {
+        var hash = 7
+        hash = 41 * hash + Objects.hashCode(refID)
+        hash = 41 * hash + Objects.hashCode(FORMS)
+        return hash
     }
 
     /**
-     * @see Object#equals()
+     * @see Object.equals
      * @return
      */
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        } else if (obj == null) {
-            return false;
-        } else if (getClass() != obj.getClass()) {
-            return false;
+    override fun equals(other: Any?): Boolean {
+        return when {
+            this === other -> {
+                true
+            }
+            other == null -> {
+                false
+            }
+            javaClass != other.javaClass -> {
+                false
+            }
+            else -> {
+                val other2 = other as ChangeFormFLST
+                refID == other2.refID && FORMS == other2.FORMS
+            }
         }
-
-        final ChangeFormFLST other = (ChangeFormFLST) obj;
-        return Objects.equals(this.FLAGS, other.FLAGS) && Objects.equals(this.FORMS, other.FORMS);
     }
 
     /**
-     * @see AnalyzableElement#getInfo(resaver.Analysis, resaver.ess.ESS)
+     * @see AnalyzableElement.getInfo
      * @param analysis
      * @param save
      * @return
      */
-    @Override
-    public String getInfo(resaver.Analysis analysis, ESS save) {
-        final StringBuilder BUILDER = new StringBuilder();
-
-        BUILDER.append("<hr/><p>FORMLIST:</p>");
-
-        if (null != this.FLAGS) {
-            BUILDER.append(String.format("<p>ChangeFormFlags: %s</p>", this.FLAGS));
+    override fun getInfo(analysis: Analysis?, save: ESS?): String {
+        val BUILDER = StringBuilder()
+        BUILDER.append("<hr/><p>FORMLIST:</p>")
+        if (null != refID) {
+            BUILDER.append(String.format("<p>ChangeFormFlags: %s</p>", refID))
         }
-
-        if (null != this.FORMS) {
-            BUILDER.append(String.format("<p>List size: %d</p><ol start=0>", this.FORMS.size()));
-            this.FORMS.forEach(refid -> {
-                if (save.getChangeForms().containsKey(refid)) {
-                    BUILDER.append(String.format("<li>%s", refid.toHTML(null)));
+        if (null != FORMS) {
+            BUILDER.append(String.format("<p>List size: %d</p><ol start=0>", FORMS!!.size))
+            FORMS!!.forEach { refid: RefID ->
+                if (save!!.changeForms.containsKey(refid)) {
+                    BUILDER.append(String.format("<li>%s", refid.toHTML(null)))
                 } else {
-                    BUILDER.append(String.format("<li>%s", refid));
+                    BUILDER.append(String.format("<li>%s", refid))
                 }
-            });
-            BUILDER.append("</ol>");
+            }
+            BUILDER.append("</ol>")
         }
-
-        return BUILDER.toString();
+        return BUILDER.toString()
     }
 
     /**
-     * @see AnalyzableElement#matches(resaver.Analysis, resaver.Mod)
+     * @see AnalyzableElement.matches
      * @param analysis
      * @param mod
      * @return
      */
-    @Override
-    public boolean matches(Analysis analysis, String mod) {
-        return false;
+    override fun matches(analysis: Analysis?, mod: String?): Boolean {
+        return false
     }
 
-    final private ChangeFormFlags FLAGS;
-    final private List<RefID> FORMS;
+    /**
+     * @return The `ChangeFormFlags` field.
+     */
+    val refID: ChangeFormFlags? = if (flags.getFlag(1)) ChangeFormFlags(input) else null
+    private var FORMS: MutableList<RefID>? = null
 
+    /**
+     * Creates a new `ChangeForm` by reading from a
+     * `LittleEndianDataOutput`. No error handling is performed.
+     *
+     * @param input The input stream.
+     * @param flags The change flags.
+     * @param context The `ESSContext` info.
+     */
+    init {
+        if (flags.getFlag(31)) {
+            val formCount = input.int
+            require(formCount <= 0x3FFF) { "Invalid data: found $formCount formCount in FLST." }
+            FORMS = mutableListOf()
+            (0 until formCount).forEach { i ->
+                val REF = context.readRefID(input)
+                FORMS!!.add(REF)
+            }
+        } else {
+            FORMS = null
+        }
+    }
 }
