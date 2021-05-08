b0cf30908979d83c6b2b88418c805c181b84496b
noah the goodra <peterpan0413@live.com>
2021-05-08 03:49:40 -0500

    converted map

--- src/jvmMain/java/ess/papyrus/ReferrentMap.kt
+++ src/jvmMain/java/ess/papyrus/ReferrentMap.kt
@@ -13,15 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ess.papyrus;
+package ess.papyrus
 
-import java.util.HashMap;
-import java.util.Set;
-import ess.Linkable;
+
+import ess.Linkable
 
 /**
  *
  */
-@SuppressWarnings("serial")
-public class ReferrentMap extends HashMap<Class<? extends Linkable>, Set<Linkable>> {
-}
+class ReferrentMap : HashMap<Class<out Linkable?>?, Set<Linkable?>?>()
