bd984f5dd5890a99fd49938f5a6dcbafb81a9067
noah the goodra <peterpan0413@live.com>
2021-05-10 18:54:20 -0500

    removed that server dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,6 @@ dependencies {
     implementation("org.openjfx:javafx-swing:15")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.0")
     implementation("org.jetbrains:annotations:20.1.0")
-    implementation("io.ktor:ktor-server-core:1.5.4")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.0")
     implementation("no.tornado:tornadofx:1.7.20")
     implementation("com.github.ajalt.clikt:clikt:3.1.0")
