10b50ce696b62747c91017823e0a2505b824978e
noah the goodra <peterpan0413@live.com>
2021-05-10 21:23:13 -0500

    added generic predicate to the utility file

--- src/commonMain/kotlin/SpecialNonSpecificBits.kt
+++ src/commonMain/kotlin/SpecialNonSpecificBits.kt
@@ -5,3 +5,4 @@ typealias specialConsumer = (String) -> Unit
 typealias MIntConsumer = (Int) -> Unit
 typealias MDoubleConsumer = (Double) -> Unit
 typealias GenericConsumer<T> = (T) -> Unit
+typealias GenericPredicate<T> = (T) -> Boolean
