f8b6f7603ca7a694b2e6931971468b29c39e3dc2
noah the goodra <peterpan0413@live.com>
2021-05-29 15:23:43 -0500

    use sumof

--- src/jvmMain/kotlin/resaver/esp/FieldVMAD.kt
+++ src/jvmMain/kotlin/resaver/esp/FieldVMAD.kt
@@ -44,17 +44,9 @@ class FieldVMAD(recordCode: RecordCode, fieldCode: IString, input: PlatformByteB
      */
     override fun calculateSize(): Int {
         var sum = 12
-        var result = 0
-        for (SCRIPT in SCRIPTS) {
-            val calculateSize = SCRIPT.calculateSize()
-            result += calculateSize
-        }
+        val result = SCRIPTS.sumOf { it.calculateSize() }
         sum += result
-        var sum1 = 0
-        for (FRAGMENT in FRAGMENTS) {
-            val calculateSize = FRAGMENT.calculateSize()
-            sum1 += calculateSize
-        }
+        val sum1 = FRAGMENTS.sumOf { it.calculateSize() }
         sum += sum1
         return sum
     }
