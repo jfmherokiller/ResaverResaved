45c93c837ce47e8adcc5b9644da364ee449fa7c3
noah the goodra <peterpan0413@live.com>
2021-05-29 17:37:08 -0500

    move Duad

--- src/commonMain/kotlin/mf/Duad.kt
+++ src/commonMain/kotlin/mf/Duad.kt
@@ -1,21 +1,4 @@
-/*
- * Copyright 2018 Mark Fairchild.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package mf
-
-
 
 /**
  *
@@ -42,7 +25,7 @@ class Duad<T>(var A: T, var B: T) {
         if (other == null) {
             return false
         }
-        if (javaClass != other.javaClass) {
+        if (this::class != other::class) {
             return false
         }
         val other2 = other as Duad<*>
